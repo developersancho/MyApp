5ecc248c98022b0720b183b562bea0966d62136e
developersancho <developersancho@gmail.com>
2019-03-09 14:52:13 +0300

    Initial commit

---
+++ app/src/development/java/Constant.kt
@@ -0,0 +1,6 @@
+import com.mobillium.myapp.BuildConfig
+
+object Constant {
+    const val base_url = BuildConfig.BASE_URL
+    const val init_url = BuildConfig.INIT_URL
+}
---
+++ app/src/main/java/com/mobillium/myapp/Constant.kt
@@ -0,0 +1,6 @@
+package com.mobillium.myapp
+
+object Constant {
+    const val base_url = BuildConfig.BASE_URL
+    const val init_url = BuildConfig.INIT_URL
+}
